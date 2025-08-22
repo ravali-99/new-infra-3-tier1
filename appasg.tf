bdf79d2f48c8126e2a2a67b5f9923e5e92295383
ravali-99 <ravali9498@gmail.com>
2025-08-22 17:36:13 +0000

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-00ca32bbc84273381"
+  image_id      = "ami-0861f4e788f5069dd"
   instance_type = "t2.micro"
   key_name      = "ravali"
 
